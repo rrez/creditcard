3bbf9ca281686aadc903ef25f3b4d4e565b0888a
Robert Rez <thebigtony@gmail.com>
2021-02-13 13:14:27 +0100

    Tutorial done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { CardComponent } from './card/card.component';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CardFormComponent,
-    InputComponent
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(options)
   ],
   providers: [],
   bootstrap: [AppComponent]
